182899998028d5890a55f893fc5b8f5ca6163628
Casey Hyde <hyde.casey@gmail.com>
2016-11-09 17:05:58 -0600

    Server works, built teamArray, append divs for each team member

--- server/app.js
+++ server/app.js
@@ -2,15 +2,29 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var teamArray = ["Phil", "Hien", "Casey"];
-
+var teamArray = [
+  {
+    name: "Casey",
+    bio: "I enjoy long walks on the beach with Phil (and Hien on occasion).",
+    image: "./public/assests/images/casey.img"
+  },
+  {
+    name: "Phil",
+    bio: "I enjoy long walks on the beach with Casey (and Hien on occasion).",
+    image: "./public/assests/images/phil.img"
+  },
+  {
+    name: "Hien",
+    bio: "I enjoy long walks on the beach by myself (and on occasion with Phil or Casey).",
+    image: "./public/assets/images/hien.img"
+  }
+];
 
 app.set('port', process.env.PORT || 3000);
 
-
 app.get('/bios', function(req, res) {
   console.log("HELLOHELLO");
-  console.log(teamArray);
+  res.send(teamArray);
 });
 
 app.get('/*', function (req, res) {
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,23 @@
+var teamArray = [];
+
+function appendToDom() {
+  for (var i = 0; i < teamArray.length; i++) {
+    console.log("hello");
+    $('#team').append('<div id ="' + teamArray[i].name + '"></div>');
+  }
+}
+
 $(document).ready(function() {
   console.log("Hello world");
 
   $.ajax({
     type: 'GET',
     url: '/bios',
-
+    success: function(array) {
+      teamArray = array;
+      console.log(teamArray);
+      appendToDom();
+    }
   });
-
-
 
 });
