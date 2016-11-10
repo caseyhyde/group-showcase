3f3dfcb4c9d9830206de8a49c0f870321162b4c6
Casey Hyde <hyde.casey@gmail.com>
2016-11-10 11:29:42 -0600

    Finish basic functionality

--- server/app.js
+++ server/app.js
@@ -1,44 +1,93 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var likes = {
+  Casey: 0,
+  Hien: 0,
+  Phil: 0
+};
 
 var teamArray = [
   {
     name: "Casey",
     bio: "I enjoy long walks on the beach with Phil (and Hien on occasion).",
-    image: "./public/assests/images/casey.img"
+    image: "./assets/images/casey.img",
+
   },
   {
     name: "Phil",
     bio: "I enjoy long walks on the beach with Casey (and Hien on occasion).",
-    image: "./public/assests/images/phil.img"
+    image: "./assets/images/phil.img",
+
+
   },
   {
     name: "Hien",
     bio: "I enjoy long walks on the beach by myself (and on occasion with Phil or Casey).",
-    image: "./public/assets/images/hien.img"
+    image: "./assets/images/hien.img",
+
   }
 ];
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/likes', function (req, res) {
+  res.send(likes);
+})
+
 app.get('/bios', function(req, res) {
-  console.log("HELLOHELLO");
+  //console.log("HELLOHELLO");
   res.send(teamArray);
 });
 
+app.post('/likes', function(req,res) {
+  console.log(req.body.name);
+  addLike(req);
+  res.send('HORSE');
+})
+
 app.get('/*', function (req, res) {
   var file = req.params[0] || '/views/index.html';
-  console.log("file " + file);
+  //console.log("file " + file);
 
   res.sendFile(path.join(__dirname, './public', file));
-  console.log("dirname" + __dirname);
+  //console.log("dirname" + __dirname);
 });
 
 
 app.listen(app.get('port'), function() {
   console.log("WORKING!");
 });
+
+function addLike(req) {
+var name = req.body.name;
+switch (name) {
+  case "Phil":
+    likes.Phil++;
+    break;
+  case "Casey":
+    likes.Casey++;
+    break;
+  case "Hien":
+    likes.Hien++;
+    break;
+  default:
+
+  }
+  console.log(likes);
+}
+
+
+
+  //
+  // console.log("req.body.name " + req.body.name);
+  // if(req.body.name === "Phil"){
+  //   likes.Phil++;
+  // }
+//   console.log(likes.Phil);
+// }
 /*
  Serve this file from an Express route that responds to a GET request for the url /.
 */
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,24 @@
 var teamArray = [];
+var likes = {};
 
 function appendToDom() {
   for (var i = 0; i < teamArray.length; i++) {
     console.log("hello");
-    $('#team').append('<div id ="' + teamArray[i].name + '"></div>');
+    $('#team').append('<div class="student" id ="' + teamArray[i].name + '"><h2>'
+    + teamArray[i].name + '</h2><p>' + teamArray[i].bio +
+    '</p><img src="' + teamArray[i].image + '"/><button>LIKE</button><p id="likes"></p></div>');
+
+    $('#team').children().last().data('name', teamArray[i].name);
+    appendLikes();
   }
 }
+
+function appendLikes () {
+  $('#Casey').children().last().text(likes.Casey);
+  $('#Hien').children().last().text(likes.Hien);
+  $('#Phil').children().last().text(likes.Phil);
+  }
+
 
 $(document).ready(function() {
   console.log("Hello world");
@@ -19,5 +32,37 @@ $(document).ready(function() {
       appendToDom();
     }
   });
+
+$('#team').on('click', 'button', function() {
+  //console.log($(this));
+  var personClicked = $(this).parent().data('name');
+  sendLikes(personClicked);
+
+
+});
+
+function sendLikes(personClicked){
+  $.ajax({
+    type: 'POST',
+    url: '/likes',
+    data: { name: personClicked},
+    success: function (data) {
+    getLikes();
+    }
+  });
+}
+
+function getLikes() {
+$.ajax ({
+  type: 'GET',
+  url: '/likes',
+  success: function (data) {
+    likes = data;
+    appendLikes();
+    }
+  });
+}
+
+
 
 });
